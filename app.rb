644ce18b57dcf636b4c0f60281775d18f28dd3f0
Raveen K Sharma <raveen08@gmail.com>
2020-09-28 21:17:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,13 +6,13 @@ class App < Sinatra::Base
   end
   
   post '/team' do
-    @coach = params[:coach]
-       <p>Point Guard: <%= #{params[:pg]} %></p>
-   <p>Shooting Guard: <%= #{params[:sg]} %></p>
-   <p>Small Forward: <%= #{params[:sf]} %></p>
-   <p>Power Forward: <%= #{params[:pf]} %></p>
-   <p>Center: <%= #{params[:c]} %></p>
-    erb :team
+   @Coach = params[:coach]
+   @PointGuard: params[:pg]
+   @ShootingGuard: params[:sg]
+   @SmallForward: params[:sf]
+   @PowerForward: params[:pf]
+   @Center: params[:c]
+   erb :team
   end
 
 end
